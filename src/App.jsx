dad70bab9d47e18f631a99d310cd6c5a706f6bf2
Melquesedek Antonio de Oliveira <melquesedekdeoliveira@gmail.com>
2022-03-05 16:30:56 -0300

    Github Explorer

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/ReposirotyList';
+import './styles/global.scss';
+export function App(){
+    //throw new Error('Eita. Deu ruim galera.');
+    return (
+        <>
+    <RepositoryList/>
+    </>
+    );
+}
---
+++ src/components/ReposirotyList.jsx
@@ -0,0 +1,28 @@
+import { RepositoryItem } from "./RepositoryItem";
+import {useState, useEffect} from "react";
+import '../styles/repositories.scss';
+//https://api.github.com/users/pmmmwh/repos
+
+export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/users/melquesedek/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    },[])
+
+    return(
+        <section className="repository-list">
+        <h1>Lista de repositórios</h1>
+        <ul>
+            {repositories.map(repository => {
+                return <RepositoryItem
+                key={repository.name}
+                repository={repository} />
+            })}
+        
+        </ul>    
+        </section>
+    )
+}
